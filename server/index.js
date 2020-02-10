10a40acb117211b98ccfef0c7900099f3166dbb7
Kaleb Lee <46289359+kayrub@users.noreply.github.com>
2020-02-10 12:26:39 -0800

    Proxy works except Christines. Not updated to branches

---
+++ server/index.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use(('/'), express.static(path.join(__dirname, '../client/dist')));
+app.use(('/:id'), express.static(path.join(__dirname, '../client/dist')));
+
+
+app.listen(port, () => console.log(`Kayrub Proxy is listening 5ever ${port}`));
